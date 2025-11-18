f79b39086165d936f241e4ae07bc88668f4aece2
rian <riandeoliveira26@gmail.com>
2025-11-18 12:15:05 -0300

    rotas quase concluidas

---
+++ produto/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('cadastrar/', views.cadastrar_produto, name='cadastrar_produto'),
+    path('listar/', views.listar_produtos, name='listar_produtos'),
+    path('editar/<int:produto_id>/', views.editar_produto, name='editar_produto'),
+    path('deletar/<int:produto_id>/', views.deletar_produto, name='deletar_produto'),
+    path('adicionar/<int:produto_id>/', views.adicionar_produto, name='adicionar_produto'),
+    path('remover/<int:produto_id>/', views.remover_estoque, name='remover_estoque'),
+]
