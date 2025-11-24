1ea19f7af0189441c7335617e92cfe26dbeb02ac
rian <riandeoliveira26@gmail.com>
2025-11-24 10:33:03 -0300

    correção do urls produto

--- produto/urls.py
+++ produto/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('cadastrar/', views.cadastrar_produto, name='cadastrar_produto'),
     path('listar/', views.listar_produtos, name='listar_produtos'),
-    path('editar/<int:produto_id>/', views.editar_produto, name='editar_produto'),
+    path('editar/', views.editar_produto, name='editar_produto'),
     path('deletar/<int:produto_id>/', views.deletar_produto, name='deletar_produto'),
     path('adicionar/', views.adicionar_produto, name='adicionar_produto'),
     path('remover/<int:produto_id>/', views.remover_estoque, name='remover_estoque'),
